dc8c289dd6f78c92aaa7d56f985cbfcf01fda8ab
Ali <75311052+AAVision@users.noreply.github.com>
2026-01-15 14:04:01 +0200

    Add the project

---
+++ resources/js/stores/GuestAddressStore.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+
+export const useGuestAddress = defineStore("guestAddress", {
+    state: () => ({
+        name: null,
+        email: null,
+        country: null,
+        phone_code: null,
+        phone: null,
+        area: null,
+        flat_no: null,
+        post_code: null,
+        address_line: null,
+        address_line2: null,
+        address_type: null,
+        langitude: null,
+        longitude: null,
+        errors: {}
+    }),
+    actions: {
+        clearGuestAddress() {
+            this.name = null;
+            this.email = null;
+            this.country = null;
+            this.phone_code = null;
+            this.phone = null;
+            this.area = null;
+            this.flat_no = null;
+            this.post_code = null;
+            this.address_line = null;
+            this.address_line2 = null;
+            this.langitude = null;
+            this.longitude = null;
+            this.errors = {};
+        },
+    },
+
+});
